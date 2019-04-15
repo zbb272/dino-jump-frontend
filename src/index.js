7e7d2df3fde0c49ffa6c6b008ff7838e5424f76f
github username <github email address>
2019-04-15 09:52:04 -0400

    refactored block to accept an object for constructor

--- src/index.js
+++ src/index.js
@@ -24,16 +24,24 @@ function run(){
   }
 
   function createTestLevel(){
-    let platform2 = new Block(0, gameContainer.clientHeight - 100, 300, 100, gameContainer);
-    let platform3 = new Block(300, gameContainer.clientHeight - 250, 50, 250, gameContainer);
-    //let platform4 = new Block(400, 200, 50, 200, gameContainer);
-    let platform5 = new Block(550, gameContainer.clientHeight - 220, 75, 220, gameContainer);
-    let platform6 = new Block(1000, gameContainer.clientHeight - 300, gameContainer.clientWidth - 1000, 300, gameContainer);
-    let platform11 = new Block(800, gameContainer.clientHeight - 200, 10, 15, gameContainer);
-    let platform7 = new Block(875, gameContainer.clientHeight - 500, 100, 15, gameContainer);
-    let platform8 = new Block(700, gameContainer.clientHeight - 500, 75, 15, gameContainer);
-    let platform9 = new Block(500, gameContainer.clientHeight - 600, 50, 15, gameContainer);
-    let platform10 = new Block(200, gameContainer.clientHeight - 650, 24, 5, gameContainer);
+    let obj = {x: 0, y: gameContainer.clientHeight - 100, width: 300, height: 100, gameContainer: gameContainer, color: "black", status: "platform"}
+    let platform2 = new Block(obj);
+    obj = {x: 300, y: gameContainer.clientHeight - 250, width: 50, height: 250, gameContainer: gameContainer, color: "black", status: "platform"}
+    let platform3 = new Block(obj);
+    obj = {x: 550, y: gameContainer.clientHeight - 220, width: 75, height: 220, gameContainer: gameContainer, color: "black", status: "platform"}
+    let platform5 = new Block(obj);
+    obj = {x: 1000, y: gameContainer.clientHeight - 300, width: gameContainer.clientWidth - 1000, height: 300, gameContainer: gameContainer, color: "black", status: "platform"}
+    let platform6 = new Block(obj);
+    obj = {x: 800, y: gameContainer.clientHeight - 200, width: 10, height: 15, gameContainer: gameContainer, color: "black", status: "platform"}
+    let platform11 = new Block(obj);
+    obj = {x: 875, y: gameContainer.clientHeight - 500, width: 100, height: 15, gameContainer: gameContainer, color: "black", status: "platform"}
+    let platform7 = new Block(obj);
+    obj = {x: 700, y: gameContainer.clientHeight - 500, width: 75, height: 15, gameContainer: gameContainer, color: "black", status: "platform"}
+    let platform8 = new Block(obj);
+    obj = {x: 500, y: gameContainer.clientHeight - 600, width: 50, height: 15, gameContainer: gameContainer, color: "black", status: "platform"}
+    let platform9 = new Block(obj);
+    obj = {x: 250, y: gameContainer.clientHeight - 650, width: 24, height: 5, gameContainer: gameContainer, color: "black", status: "platform"}
+    let platform10 = new Block(obj);
   }
 
   //event handlers

be8ba89a8543a59c958d8c5a540114cdfbff6aba
github username <github email address>
2019-04-16 14:57:35 -0400

    added main menu

--- src/index.js
+++ src/index.js
@@ -5,137 +5,162 @@ document.addEventListener("DOMContentLoaded", run());
 
 function run() {
   gameContainer = document.getElementById("game-container");
+  gameContainer.style.display = "none";
 
-  let rightPressed = false;
-  let leftPressed = false;
-  let jumpPressed = false;
-  let dashPressed = false;
-  document.addEventListener("keydown", keyDownEventHandler);
-  document.addEventListener("keyup", keyUpEventHandler);
+  let mainMenu = document.getElementById("main-menu");
+  let titleElement = document.createElement("h1");
+  titleElement.innerText = "Platform Party 6000";
+  let subTitleElement = document.createElement("h4");
+  subTitleElement.innerText = "Press Any Key To Continue";
 
-  let startY = gameContainer.clientHeight - 50;
-  let startX = 30;
-  let player = new Player(0, 0, gameContainer);
-  // let currentLevel;
+  mainMenu.appendChild(titleElement);
+  mainMenu.appendChild(subTitleElement);
+  mainMenu.addEventListener("click", mainMenuKeyDownEventHandler);
 
-  let level = "";
-  let levelUrl = `http://localhost:3000/api/v1/levels/`;
-
-  init();
-  setInterval(draw, 20);
-
-  function getLevels() {
-    return fetch(levelUrl)
-      .then(res => res.json())
-      .then(levels => {
-        levels.forEach(levelData => {
-          const level = new Level(levelData, gameContainer);
-        });
-      })
-      .then(() => {
-        levelForm();
-      });
+  function mainMenuKeyDownEventHandler(event){
+    gameListMenu();
   }
 
-  function init() {
-    getLevels().then(() => {
-      currentLevel = Level.all[0];
-      currentLevel.render();
-      // debugger;
-      player.setLevel(currentLevel);
-      player.disabled = false;
-    });
+  function gameListMenu(){
+    mainMenu.removeEventListener("click", mainMenuKeyDownEventHandler);
+
+    subTitleElement.innerText = "Select Game To Play";
+    gameList = document.createElement("ul");
+    let gameListItem = document.createElement("li");
+    gameListItem.innerText = "Test Game";
+    gameListItem.addEventListener("click", gameSelectEventListener);
+    gameList.appendChild(gameListItem);
+    mainMenu.appendChild(gameList);
   }
 
-  function levelForm() {
-    const form = document.getElementById("level-select-form");
-    const select = document.getElementById("level-select");
-
-    Level.all.forEach(level => {
-      const option = document.createElement("option");
-      option.name = level.name;
-      option.dataset.id = level.id;
-      option.innerHTML = level.name;
-      select.appendChild(option);
-    });
-    form.addEventListener("submit", e => {
-      e.preventDefault();
-
-      selectLevel(select.options[select.selectedIndex].dataset.id);
-    });
+  function gameSelectEventListener(event){
+    gameList.remove();
+    levelListMenu();
   }
-  function selectLevel(id) {
-    fetch(levelUrl + id)
+
+  function levelListMenu(){
+    subTitleElement.innerText = "Select Level To Play";
+    let levelList = document.createElement("ul");
+
+    let levelUrl = `http://localhost:3000/api/v1/levels/`;
+    fetch(levelUrl)
       .then(res => res.json())
       .then(data => {
-        const level = new Level(data, gameContainer);
-        level.render();
-        currentLevel.drop();
-        player.level = level;
-        player.setLevel(level);
-        currentLevel = level;
-      });
+        console.log(data);
+        data.forEach(level => {
+          let levelListItem = document.createElement("li");
+          levelListItem.innerText = level.name;
+          levelListItem.dataset.id = level.id;
+          levelListItem.addEventListener("click", levelSelectEventListener);
+          levelList.appendChild(levelListItem);
+          new Level(level, gameContainer)
+        })
+      })
+
+    mainMenu.appendChild(levelList);
+
   }
 
-  function draw() {
-    // checkCollision();
-    if (!player.disabled) {
-      player.draw({ left: leftPressed, right: rightPressed, jump: jumpPressed, dash: dashPressed });
-      if (dashPressed) {
-        dashPressed = false;
+  function levelSelectEventListener(event){
+    console.log(event.target)
+    //setup Player
+    let startY = gameContainer.clientHeight - 50;
+    let startX = 30;
+    player = new Player(0, 0, gameContainer);
+
+    //setup level
+    currentLevel = Level.all.find(level => level.id === parseInt(event.target.dataset.id));
+    currentLevel.render();
+    player.setLevel(currentLevel);
+    player.disabled = false;
+
+    //close menu
+    mainMenu.style.display = "none";
+    runGame();
+  }
+
+
+
+  function runGame(){
+    // gameContainer = document.getElementById("game-container");
+    gameContainer.style.display = "block";
+
+    let rightPressed = false;
+    let leftPressed = false;
+    let jumpPressed = false;
+    let dashPressed = false;
+    document.addEventListener("keydown", keyDownEventHandler);
+    document.addEventListener("keyup", keyUpEventHandler);
+
+
+    // let currentLevel;
+
+    let level = "";
+    let levelUrl = `http://localhost:3000/api/v1/levels/`;
+
+    setInterval(draw, 20);
+
+    function draw() {
+      // checkCollision();
+      if (!player.disabled) {
+        player.draw({ left: leftPressed, right: rightPressed, jump: jumpPressed, dash: dashPressed });
+        if (dashPressed) {
+          dashPressed = false;
+        }
+      }
+    }
+
+    //event handlers
+    function keyDownEventHandler(event) {
+      // event.preventDefault();
+      if (event.key === "Right" || event.key === "ArrowRight") {
+        rightPressed = true;
+        console.log("right");
+      } else if (event.key === "left" || event.key === "ArrowLeft") {
+        leftPressed = true;
+      }
+      if (event.keyCode === 32 || event.key === "Space") {
+        jumpPressed = true;
+      }
+      if (event.keyCode === 16) {
+        dashPressed = true;
+      }
+      if (event.keyCode === 76 || event.key === "l") {
+        if (!levelBuilderOpen) {
+          levelBuilderOpen = true;
+          // debugger
+          levelBuilder();
+        }
+      } else if (event.key === "?") {
+        debugger;
+      }
+
+      //temporary power toggles
+      if (event.key === "w") {
+        player.wallJumpUnlocked = !player.wallJumpUnlocked;
+      }
+      if (event.key === "j") {
+        player.doubleJumpUnlocked = !player.doubleJumpUnlocked;
+      }
+      if (event.key === "d") {
+        player.dashUnlocked = !player.dashUnlocked;
+      }
+    }
+
+    function keyUpEventHandler(event) {
+      // event.preventDefault();
+      if (event.key === "Right" || event.key === "ArrowRight") {
+        rightPressed = false;
+        player.haltRight();
+      } else if (event.key === "left" || event.key === "ArrowLeft") {
+        leftPressed = false;
+        player.haltLeft();
+      }
+      if (event.keyCode === 32 || event.key === "Space") {
+        jumpPressed = false;
+        player.haltJump();
       }
     }
   }
 
-  //event handlers
-  function keyDownEventHandler(event) {
-    // event.preventDefault();
-    if (event.key === "Right" || event.key === "ArrowRight") {
-      rightPressed = true;
-      console.log("right");
-    } else if (event.key === "left" || event.key === "ArrowLeft") {
-      leftPressed = true;
-    }
-    if (event.keyCode === 32 || event.key === "Space") {
-      jumpPressed = true;
-    }
-    if (event.keyCode === 16) {
-      dashPressed = true;
-    }
-    if (event.keyCode === 76 || event.key === "l") {
-      if (!levelBuilderOpen) {
-        levelBuilderOpen = true;
-        // debugger
-        levelBuilder();
-      }
-    } else if (event.key === "?") {
-      debugger;
-    }
-
-    //temporary power toggles
-    if (event.key === "w") {
-      player.wallJumpUnlocked = !player.wallJumpUnlocked;
-    }
-    if (event.key === "j") {
-      player.doubleJumpUnlocked = !player.doubleJumpUnlocked;
-    }
-    if (event.key === "d") {
-      player.dashUnlocked = !player.dashUnlocked;
-    }
-  }
-
-  function keyUpEventHandler(event) {
-    // event.preventDefault();
-    if (event.key === "Right" || event.key === "ArrowRight") {
-      rightPressed = false;
-      player.haltRight();
-    } else if (event.key === "left" || event.key === "ArrowLeft") {
-      leftPressed = false;
-      player.haltLeft();
-    }
-    if (event.keyCode === 32 || event.key === "Space") {
-      jumpPressed = false;
-      player.haltJump();
-    }
-  }
 }
